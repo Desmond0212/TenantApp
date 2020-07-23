bbccd0ebe41dd5060c202f0fc1d116d3d406e543
Desmond Wong <desmond.9602@gmail.com>
2020-07-23 23:01:18 +0800

    + Refactored Code and Layout.

---
+++ app/src/main/java/com/example/tenantyapp/DefectDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tenantyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DefectDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_defect_detail)
+    }
+}
